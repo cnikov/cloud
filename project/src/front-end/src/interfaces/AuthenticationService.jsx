e64df6cd413a25c31118e58fe6c81f7bba1c2408
chlohest <61017549+chlohest@users.noreply.github.com>
2021-10-24 16:57:17 +0200

    up

--- project/src/front-end/src/interfaces/AuthenticationService.jsx
+++ project/src/front-end/src/interfaces/AuthenticationService.jsx
@@ -1,38 +1,49 @@
+import axios from 'axios' // we use this library as HTTP client
+// you can overwrite the URI of the authentication microservice
+// with this environment variable
+const url = 'http://192.168.226.100:3001' || 'http://localhost:3000'
+
 class AuthenticationService {
-  setHandlers (onSucc, setAuthStatus, changeRoute) {
+  // setters
+  setHandlers(onSucc, setAuthStatus, changeRoute) {
     this.onSucc = onSucc
     this.setAuthStatus = setAuthStatus
     this.changeRoute = changeRoute
   }
-  registerUser (data, onErr) {
+  // POST /user
+  // ${data} is a JSON object with the fields
+  // username=[string] and [password]. These fields
+  // matches the specification of the POST call
+  registerUser(data, onErr) {
     window.localStorage.setItem('username', JSON.stringify(data.username))
-    window.localStorage.setItem('password', JSON.stringify(data.password))
-    this.setAuthStatus(true)
-    this.onSucc('You successfully registered!')
-    this.changeRoute('/')
-  }
-  loginUser (data, onErr) {
-    if (data.username === 'admin' && data.password === 'admin') {
-      window.localStorage.setItem('username', 'admin')
-      window.localStorage.setItem('password', 'admin')
-      this.setAuthStatus(true)
-      this.onSucc('You successfully logged in!')
-      this.changeRoute('/')
-      return
-    }
-    var user = window.localStorage.getItem('username')
-    if (user && user === JSON.stringify(data.username)) {
-      var passw = window.localStorage.getItem('password')
-      if (passw === JSON.stringify(data.password)) {
+    axios.post(`${url}/user`, data) // Perform an HTTP POST rquest to a url with the provided data.
+      .then((res) => {
+        // we keep the authentication token
+        window.localStorage.setItem('authToken', JSON.stringify(res.data.token))
         this.setAuthStatus(true)
-        this.onSucc('You successfully logged in!')
+        this.onSucc(`Successful registration of user [${data.username}]!`)
         this.changeRoute('/')
-      } else {
-        onErr("Your password doesn't match!")
-      }
-    } else {
-      onErr(`User [${data.username}] do not have an account.`)
-    }
+      })
+      .catch((error) => {
+        console.error(error.message)
+        var msg = `Registration of user [${data.username}] failed.`
+        onErr(`${msg} Error: ${error.msg}`)
+      })
+  }
+  // GET /user/:username/:password
+  loginUser(data, onErr) {
+    window.localStorage.setItem('username', JSON.stringify(data.username))
+    axios.get(`${url}/user/${data.username}/${data.password}`) // Perform an HTTP GET rquest to a url.
+      .then((res) => {
+        window.localStorage.setItem('authToken', JSON.stringify(res.data.token))
+        this.setAuthStatus(true)
+        this.onSucc(`Welcome back [${data.username}]!`)
+        this.changeRoute('/')
+      })
+      .catch((error) => {
+        console.error(error.message)
+        onErr(`User [${data.username}] is not registered or his credentials are incorrect.`)
+      })
   }
 }
 
