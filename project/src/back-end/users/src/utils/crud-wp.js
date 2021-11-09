854558a691a320a9550cdd6f3b3ab76796e0ecb4
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-09 01:29:04 +0100

    Update crud-wp.js

--- project/src/back-end/users/src/utils/crud-wp.js
+++ project/src/back-end/users/src/utils/crud-wp.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcryptjs')
 const tku = require('./en-de-coders')
 
-var users = require('nano')(process.env.DB_URL)
+var users = require('nano')(process.env.DB_URL_USER)
+var kart =  require('nano')(process.env.DB_URL_KART)
+var kart_id = Math.random().toString(36).slice(2)
 
 function equalPassws (usrPass, usrDbPass) {
   return bcrypt.compareSync(usrPass, usrDbPass)
@@ -13,7 +15,7 @@ function createUser (usrName, passw) {
       // 1st argument of nano.insert()
       { 'passw': bcrypt.hashSync(passw, bcrypt.genSaltSync()),
 	'permission': "user",
-	'kart-id': Math.random().toString(36).slice(2),
+	'kart-id': kart_id,
  },
       usrName, // 2nd argument of nano.insert()
       // callback to execute once the request to the DB is complete
@@ -27,6 +29,19 @@ function createUser (usrName, passw) {
         }
       }
     )
+kart.insert({
+'kart-id': kart_id,
+'items': ""
+}
+	    (error, success) => {
+        if (success) {
+        } else {
+          reject(
+            new Error(`In the creation of user (${usrName}). Reason: ${error.reason}.`)
+          )
+        }
+      }
+)
   })
 }
 
