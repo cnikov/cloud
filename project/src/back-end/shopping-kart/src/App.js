95c2514ebcda8366abcbdb19a2a3dc913d1b0a2e
chlohest <charles.lohest@student.uclouvain.be>
2021-11-10 14:29:03 +0000

    ajout microservice panier

---
+++ project/src/back-end/shopping-kart/src/App.js
@@ -0,0 +1,32 @@
+//const express = require('express')
+//const log = require('debug')('users-d')
+
+const appS = express.Router()
+//const db = process.env.WITH_PERSISTENT_DATA ? require('./utils/crud-wp') : require('./utils/crud')
+
+appS.post('/shop', (req, res) => {
+  var id = req.body.id
+  var item = req.body.item
+  log(`Adding a new item (${item}) identified in basket "${id}"`)
+  return db.AddToBasket(id, item)
+    .then((token) => {
+      res.status(200).json({ status: 'success', token })
+    })
+    .catch((err) => {
+      res.status(409).json({ status: 'error', message: String(err) })
+    })
+})
+
+app.get('/shop/:id', (req, res) => {
+  var id = req.params.id
+  log(`Getting basket (${id})`)
+  return db.getBasket(id)
+    .then((token) => {
+      res.status(200).json({ status: 'success', token })
+    })
+    .catch((err) => {
+      res.status(404).json({ status: 'error', message: String(err) })
+    })
+})
+
+module.exports = App
---
+++ project/src/back-end/shopping-kart/src/utils/crud.js
@@ -0,0 +1,74 @@
+const bcrypt = require('bcryptjs')
+const tku = require('./en-de-coders')
+
+var users = require('nano')(process.env.DB_URL)
+var kart = require('nano')(process.env.DB_URL_KART)
+
+
+function equalPassws(usrPass, usrDbPass) {
+  return bcrypt.compareSync(usrPass, usrDbPass)
+}
+
+
+function createUser(usrName, passw) {
+
+  return new Promise((resolve, reject) => {
+    users.insert(
+      // 1st argument of nano.insert()
+      {
+        'passw': bcrypt.hashSync(passw, bcrypt.genSaltSync()),
+
+      },
+      usrName, // 2nd argument of nano.insert()
+      // callback to execute once the request to the DB is complete
+      (error, success) => {
+        if (success) {
+          resolve(tku.encodeToken(usrName))
+        } else {
+          reject(
+            new Error(`In the creation of user (${usrName}). Reason: ${error.reason}.`)
+          )
+        }
+      }
+    )
+    kart.insert({
+      'kart-id': kart_id,
+      'items': ""
+    })
+
+  })
+}
+
+function getUser(usrName, passw) {
+  return new Promise((resolve, reject) => {
+    users.get(usrName, (error, success) => {
+      if (success) {
+        if (!equalPassws(passw, success.passw)) {
+          reject(new Error(`Passwords (for user: ${usrName}) do not match.`))
+        }
+        resolve(tku.encodeToken(usrName))
+      } else {
+        reject(new Error(`To fetch information of user (${usrName}). Reason: ${error.reason}.`))
+      }
+    })
+  })
+}
+// function getKart (usrName) {
+//   return new Promise((resolve, reject) => {
+//     kart.get("databaseName", "some_document_id").then(({data, headers, status}) => {
+//       // data is json response
+//       // headers is an object with all response headers
+//       // status is statusCode number
+//   }, err => {
+//       // either request error occured
+//       // ...or err.code=EDOCMISSING if document is missing
+//       // ...or err.code=EUNKNOWN if statusCode is unexpected
+//   });})
+//   })
+// }
+
+module.exports = {
+  createUser,
+  getUser,
+  //getKart
+}
