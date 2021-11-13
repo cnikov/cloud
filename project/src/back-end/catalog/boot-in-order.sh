0a61e93e773667cf3991fa25444c2cb589c0639e
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-13 11:34:00 +0100

    bootinorder

--- project/src/back-end/catalog/boot-in-order.sh
+++ project/src/back-end/catalog/boot-in-order.sh
@@ -30,28 +30,5 @@ done
 echo "DB (${DB_NAME_C}) was created!"
 echo "Start users service..."
 
-source post_db.sh ${DB_URL_C}
 
-
-echo "Apply a formatter for each view"
-mkdir formatter_output
-DEBUG=views* node func_to_string.js
-if [[ ${?} != 0 ]]; then
-  echo -e "ERROR: during the creation of views\nEND OF \{0}"
-  exit 1
-fi
-echo -e "\tDONE"
-
-cd formatter_output
-echo "Creation of views for users DB"
-for view in `ls *.js`; do
-  curl --request PUT "${DB_URL_C}/_design/queries" --upload-file ${view}
-  if [[ ${?} != 0 ]]; then
-    echo -e "ERROR: during the creation of view ${view}\nEND OF \{0}"
-    exit 1
-  fi
-done
-echo -e "\tDONE"
-
-echo "END OF ${0}"
 npm start
