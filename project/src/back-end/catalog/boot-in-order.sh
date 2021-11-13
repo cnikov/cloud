d15ca8733ffe6323fccd03844626ba13458577a1
RomTourpe <62745994+RomTourpe@users.noreply.github.com>
2021-11-13 23:07:30 +0100

    e

--- project/src/back-end/catalog/boot-in-order.sh
+++ project/src/back-end/catalog/boot-in-order.sh
@@ -26,7 +26,6 @@ until curl --request PUT ${DB_URL_C} ; do
   echo -e "\t DB (${DB_NAME_C}) wasn't created - trying again later..."
   sleep 2
 done
-npm start
 #until curl -X POST --data "id=1&name=chou&price=2&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg&category=non" http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/catalog; do
 #sleep 2
 #done
