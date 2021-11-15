a078080894480c0e03214e84961d807016b390e8
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-15 19:06:52 +0100

    fix

--- project/src/back-end/catalog/src/utils/crud.js
+++ project/src/back-end/catalog/src/utils/crud.js
@@ -15,7 +15,7 @@ function AddProduct(name, price, image, category, id) {
       }
     }
     var catalogs = 'catalog'
-    const dblist = catalog.info('catalog')
+    const dblist = catalog.catalog.info()
     console.log(dblist)
     if (dblist != null) {
 
