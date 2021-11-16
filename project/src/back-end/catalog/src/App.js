3c5ae0622e99ac39b1edb25471e241601e04d28b
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-16 18:21:49 +0100

    post

--- project/src/back-end/catalog/src/App.js
+++ project/src/back-end/catalog/src/App.js
@@ -2,17 +2,19 @@ const express = require('express')
 const log = require('debug')('users-d')
 const app = express.Router()
 const db = require('./utils/crud')
-app.post('/listitem', (req, res) => {
 
+
+
+
+app.post('/listitem', (req, res) => {
   var name = req.body.name
   log(`Adding a new item (${name}) identifie`)
 
   return db.FillTheList(name).then((token) => {
     res.status(200).json({ status: 'success', token })
+  }).catch((err) => {
+    res.status(409).json({ status: 'error', message: String(err) })
   })
-    .catch((err) => {
-      res.status(409).json({ status: 'error', message: String(err) })
-    })
 })
 app.post('/catalog', (req, res) => {
   var name = req.body.name
