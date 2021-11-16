5f3901c84bae1795db2ec12c5ff8949e9dda8104
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-16 11:47:21 +0100

    get

--- project/src/back-end/catalog/src/App.js
+++ project/src/back-end/catalog/src/App.js
@@ -23,7 +23,7 @@ app.post('/catalog', (req, res) => {
 
 app.get('/catalog', (req, res) => {
 
-  var dbid = res.params.db_name
+  var dbid = res.params
   return db.getProduct(dbid)
     .then((token) => {
       res.status(200).json({ status: 'success', token })
