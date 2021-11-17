cd07a5e91b56f49fbbdc5dd4d6e0c5d01ddbe2f3
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-17 22:45:28 +0100

    delete

--- project/remove_item.sh
+++ project/remove_item.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -
-curl -X REMOVE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/catalog/$1
-curl -X REMOVE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/format/$1
-curl -X REMOVE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/listitem/$1
+curl -X DELETE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/catalog/$1
+curl -X DELETE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/format/$1
+curl -X DELETE http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/listitem/$1
