887de2725dc37c5b28ad183660685496db5cf033
RomTourpe <62745994+RomTourpe@users.noreply.github.com>
2021-11-17 12:37:24 +0100

    e

--- project/src/front-end/src/shopping-cart/ShoppingCartApp.jsx
+++ project/src/front-end/src/shopping-cart/ShoppingCartApp.jsx
@@ -16,7 +16,15 @@ class ShoppingCartApp extends Component {
 
   componentWillMount() { //gerer si un user a deja un panier ou non
     console.log(JSON.parse(window.localStorage.getItem('username')))
-    this.initialiseState(true)
+    var user = JSON.parse(window.localStorage.getItem('username'))
+    if(user == null){
+      console.log("Not connected, creating an empty basket")
+      this.initialiseState(true)
+    }else{
+      console.log("well connected, loading your basket")
+      this.initialiseState(false)
+    }
+    //this.initialiseState(true)
   }
   constructor(props) {
     super(props)
