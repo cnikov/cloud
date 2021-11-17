f58df3a8d11efcbe3b8d5e9d7deec3c7adb3d7de
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-17 04:02:33 +0100

    boot

--- project/src/back-end/catalog/boot-in-order.sh
+++ project/src/back-end/catalog/boot-in-order.sh
@@ -30,6 +30,10 @@ until curl --request PUT ${DB_URL_F} ; do
   echo -e "\t DB (${DB_NAME_F}) wasn't created - trying again later..."
   sleep 2
 done
+until curl --request PUT ${DB_URL_L} ; do
+  echo -e "\t DB (${DB_NAME_L}) wasn't created - trying again later..."
+  sleep 2
+done
 #until curl -X POST --data "id=1&name=chou&price=2&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg&category=non" http://admin:admin@cloud-romtourpe.westeurope.cloudapp.azure.com:3005/catalog; do
 #sleep 2
 #done
