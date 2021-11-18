25945f8c6a9710de47f21e50aa156ab3e0fcf7c9
chlohest <61017549+chlohest@users.noreply.github.com>
2021-11-18 16:21:43 +0100

    script

--- project/add_cart.sh
+++ project/add_cart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -
 ip4=$(hostname -I | awk '{print $1}' )
-curl -X POST --data "username=hey&password=hey" http://admin:admin@$ip4:3002/user
+curl -X POST --data "username=$3&password=$3" http://admin:admin@$ip4:3002/user
 curl -X POST --data "name=$1&quantity=$2&username=$3&price=$4&id=$5" http://admin:admin@$ip4:3006/shopping-kart
