f3fe9ec2fad78ecdda16d6f0c31a02199f9389ea
RomTourpe <62745994+RomTourpe@users.noreply.github.com>
2021-11-26 18:43:55 +0100

    e

--- project/src/back-end/shopping-kart/src/utils/crud.js
+++ project/src/back-end/shopping-kart/src/utils/crud.js
@@ -1,10 +1,83 @@
+import axios from 'axios' // we use this library as HTTP client
 var kart = require('nano')(process.env.DB_URL_SK)
-var catalog = require('nano')(process.env.DB_URL_C)
-
+var catalog = require('nano')(process.env.DB_URL_C) // Appeler avec axios :(
+var url = "http://cloud-romtourpe.westeurope.cloudapp.azure.com:3005"
 //Add an item to the user's basket
 function AddToBasket(name, quantity, username, price, id) { 
 
   return new Promise((resolve, reject) => {
+    axios.get(`${url}/catalog/${name}`)
+      .then((res) => {
+        var new_basket
+      if(res){
+        kart.get(username, (error, success) => {  //on recupère le panier
+          if(success){  //si le panier existe deja
+            var index = success.name.indexOf(name)
+            console.log(index)
+            console.log("cetait l'index")
+            if (index <= -1){  //si l'objet n'est pas déja dans la db
+              success.name.push(name)
+              success.quantity.push(quantity)
+              success.image.push(res.image)
+              success.price.push(price)
+              success.id.push(id)
+              new_basket = {
+                '_rev': success._rev,
+                'name': success.name,
+                'quantity': success.quantity,
+                'image': success.image,
+                'price': success.price,
+                'id': success.id
+              }
+            }else{
+              console.log("tests sur les qtitess")
+              console.log(index)
+              console.log(success.quantity[index])
+              success.quantity[index] = quantity + success.quantity[index]
+              new_basket = {
+                '_rev':  success._rev,
+                'name': success.name,
+                'quantity': success.quantity,
+                'image': success.image,
+                'price': success.price,
+                'id': success.id
+              }
+
+            }
+          }else{  //sinon, on crée le panier
+            var nameList = []
+            nameList.push(name)
+            var quantityList = []
+            quantityList.push(quantity)
+            var imageList = []
+            imageList.push(res.image)
+            var priceList = []
+            priceList.push(res.price)
+            var idList = []
+            idList.push(res.id)
+            new_basket = {
+              'name': nameList,
+              'quantity': quantityList,
+              'image': imageList,
+              'price': priceList,
+              'id': idList
+            }
+          }
+          kart.insert(new_basket, username, (error, suc) => {
+            if(suc){
+              resolve(username)
+            }else{
+              reject(new Error("Erreur d'ajout a la db"))
+            }
+          })
+
+        })
+      }else{
+        reject(new Error("Erreur"))
+      }
+    })
+      })}
+/*
     catalog.get(name, (err, succ)=>{  //on récupère le catalogue
       var new_basket
       if(succ){
@@ -76,7 +149,7 @@ function AddToBasket(name, quantity, username, price, id) {
     })
     
   })
-}
+}*/
 //Recuperer le panier d'un user
 function getBasket(username) {
   console.log(username)
