ad21298179b0b6dd7f6b160c1c2e4689a9545a9a
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-02 17:31:07 +0100

    fix

--- project/add_history.sh
+++ project/add_history.sh
@@ -2,4 +2,4 @@
 ip4=$(hostname -I | awk '{print $1}' )
 curl -X POST --data "username=hey&password=hey" http://admin:admin@$ip4:3002/user
 curl -X POST --data "name=$1&quantity=$2&username=$3&price=$4&id=$5" http://admin:admin@$ip4:3006/shopping-kart
-curl -X POST http://admin:admin@$ip4:3008/history/$1
+curl -X POST http://admin:admin@$ip4:3008/history/$3
