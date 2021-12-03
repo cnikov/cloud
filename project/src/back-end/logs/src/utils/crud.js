4629251397bfd5c819476d2d5a6597b40f177c84
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-03 02:32:57 +0100

    logs

--- project/src/back-end/logs/src/utils/crud.js
+++ project/src/back-end/logs/src/utils/crud.js
@@ -25,7 +25,7 @@ function PostlogsRec(item, list1, list2) {
               success['value'][item]['quantity'].push(list2[i])
             }
             else {
-              ToUpdate[updIndex] = ToUpdate[updIndex] + list2[i]
+              success['value'][item]['quantity'] = success['value'][item]['quantity'] + list2[i]
             }
           }
           success['value'][item]['with'] = ToUpdate
