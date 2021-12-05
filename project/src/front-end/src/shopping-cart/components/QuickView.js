efa7e79a47aa320e5dd0eb37e43975c0f1aa62f7
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-05 15:14:06 +0100

    fin

--- project/src/front-end/src/shopping-cart/components/QuickView.js
+++ project/src/front-end/src/shopping-cart/components/QuickView.js
@@ -53,7 +53,7 @@ class QuickView extends Component {
               {console.log(recomm[name])}
               <h3>Customers who bought this item also bought</h3>
 
-              <p> {recomm[name][0]} </p>
+              <p> {recomm[name]['with'][0]} </p>
 
 
             </center>
