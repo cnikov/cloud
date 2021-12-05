8ae3b2db6aaaaa45c1592a42ab3654efc21b60c8
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-05 11:16:57 +0100

    recomm

--- project/src/front-end/src/shopping-cart/components/QuickView.js
+++ project/src/front-end/src/shopping-cart/components/QuickView.js
@@ -1,41 +1,50 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import axios from 'axios' // we use this library as HTTP client
+// you can overwrite the URI of the authentication microservice
+// with this environment variable
+const url = "http://cloud-romtourpe.westeurope.cloudapp.azure.com:3010"
 
 class QuickView extends Component {
-  componentWillUnmount () {
+  componentWillUnmount() {
     this.props.closeModal()
   }
 
-  handleClose () {
+  handleClose() {
     this.props.closeModal()
   }
 
-  render () {
+  render() {
     let product = this.props.product
     let name = product.name
     let image = product.image
     let price = product.price
-    return (
-      <div className={this.props.openModal ? 'modal-wrapper active' : 'modal-wrapper'}>
-        <div className='modal' ref='modal'>
-          <button type='button' className='close' onClick={this.handleClose.bind(this)}>&times;</button>
-          <center>
-            <div className='product'>
-              <span className='product-name'>{name}</span>
-              <br />
-              <span className='product-price'>{price}</span>
-              <div className='product-image'>
-                <img src={image} alt={name} />
+    axios.get(`${url}/logs/recommendation`).then((succ) => {
+      return (
+        <div className={this.props.openModal ? 'modal-wrapper active' : 'modal-wrapper'}>
+          <div className='modal' ref='modal'>
+            <button type='button' className='close' onClick={this.handleClose.bind(this)}>&times;</button>
+            <center>
+              <div className='product'>
+                <span className='product-name'>{name}</span>
+                <br />
+                <span className='product-price'>{price}</span>
+                <div className='product-image'>
+                  <img src={image} alt={name} />
+                </div>
               </div>
-            </div>
-            <h2>About the product</h2>
-            <p>TODO: write down small description...</p>
-            <br />
-            <h3>Customers who bought this item also bought</h3>
-            <p>TODO: fetch recommendations</p>
-          </center>
+              <h2>About the product</h2>
+              <p>{name}</p>
+              <br />
+              <h3>Customers who bought this item also bought</h3>
+              <p>{succ['value'][name]['with']}</p>
+            </center>
+          </div>
         </div>
-      </div>
-    )
+      )
+
+    })
+
+
   }
 }
 
