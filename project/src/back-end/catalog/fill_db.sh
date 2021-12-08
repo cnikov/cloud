bafa19ca48f4c8594d5251d2c486cbc618da750e
chlohest <61017549+chlohest@users.noreply.github.com>
2021-12-08 14:12:48 +0100

    script

--- project/src/back-end/catalog/fill_db.sh
+++ project/src/back-end/catalog/fill_db.sh
@@ -1,48 +1,9 @@
 #!/bin/bash -
 ip4=$(hostname -I | awk '{print $1}' )
-until curl -X POST --data "name=Brocolli&price=2&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg&category=Vegetable&id=1" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 5
-done
-until curl --request POST --data "name=Cauliflower&price=6&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg&category=Vegetable&id=2" http://admin:admin@$ip4:3005/catalog ;do 
-    echo "En attente de la creation de la db"
-sleep 1
-done
-until curl --request POST --data "name=Cauliflower&price=6&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg&category=Vegetable&id=2" http://admin:admin@$ip4:3005/catalog ;do 
-    echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Cauliflower&price=6&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg&category=Vegetable&id=2" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Cucumber&price=5.5&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cucumber.jpg&category=Vegetable&id=3" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Beetroot&price=8.7&image=https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/beetroot.jpg&category=Vegetable&id=4" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Apple&price=2.34&image=https://res.cloudinary.com/sivadass/image/upload/v1493620045/dummy-products/apple.jpg&category=Fruits&id=6" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Banana&price=1.69&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/banana.jpg&category=Fruits&id=7" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Grapes&price=5.98&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/grapes.jpg&category=Fruits&id=8" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
-until curl -X POST --data "name=Mango&price=6.80&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/mango.jpg&category=Fruits&id=9" http://admin:admin@$ip4:3005/catalog ;do
-  echo "En attente de la creation de la db"
-  sleep 1
-done
+
 until curl -X POST --data "name=Brocolli&price=2&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/broccoli.jpg&category=Vegetable&id=1" http://admin:admin@$ip4:3005/format ;do
   echo "En attente de la creation de la db"
-  sleep 1
+  sleep 5
 done
 until curl -X POST --data "name=Cauliflower&price=6&image=https://res.cloudinary.com/sivadass/image/upload/v1493620046/dummy-products/cauliflower.jpg&category=Vegetable&id=2" http://admin:admin@$ip4:3005/format ;do
   echo "En attente de la creation de la db"
@@ -72,35 +33,35 @@ until curl -X POST --data "name=Mango&price=6.80&image=https://res.cloudinary.co
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Mango" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Brocolli","list1":['Brocolli','Banana','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Grapes" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Cauliflower","list1":['Cauliflower','Banana','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Apple" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Cucumber","list1":['Cucumber','Banana','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Mango" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Beetroot","list1":['Beetroot','Banana','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Beetroot" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Apple","list1":['Apple','Banana','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Cauliflower" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Banana","list1":['Banana','Apple','Mango','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Cucumber" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Mango","list1":['Mango','Banana','Apple','Grapes'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
-until curl -X POST --data "name=Brocolli" http://admin:admin@$ip4:3005/listitem ;do
+until curl -X POST --data ''{"item":"Grapes","list1":['Grapes','Banana','Mango','Apple'],"list2":[1,2,3,4]}'' http://admin:admin@$ip4:3010/recommendation ;do
   echo "En attente de la creation de la db"
   sleep 1
 done
