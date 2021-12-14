4569f567f2be6068c13a83455c47ecb7c8de4157
chlohest <charles.lohest@student.uclouvain.be>
2021-12-14 16:33:53 +0100

    up

--- project/src/back-end/logs/views/myview.js
+++ project/src/back-end/logs/views/myview.js
@@ -2,7 +2,7 @@ const viewDescriptor = {
     views: {
       logs_per_category: {
         map: function (doc) {
-          if (doc['type'].localeCompare("recommendation")){
+          if (doc['type'].localeCompare("recommendation") ==0){
             for(var user in doc.value){
               emit(user, doc['value'][user]);
             }
