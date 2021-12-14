938d30fe9f5c8eae10f7f7027d59fa8347f4fc5a
chlohest <charles.lohest@student.uclouvain.be>
2021-12-14 01:17:58 +0100

    script

--- project/add_item.sh
+++ project/add_item.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -
 ip4=$(hostname -I | awk '{print $1}' )
-curl -X POST --data "name=$1&price=$2&image=$3&category=$4&id=$5" http://admin:admin@$ip4:3005/catalog
 curl -X POST --data "name=$1&price=$2&image=$3&category=$4&id=$5" http://admin:admin@$ip4:3005/format
-curl -X POST --data "name=$1" http://admin:admin@$ip4:3005/listitem
