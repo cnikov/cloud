19c6ca613afaf7aef7ff172b484daf3ded9c45b3
chlohest <charles.lohest@student.uclouvain.be>
2021-12-14 16:48:18 +0100

    up

--- project/src/back-end/logs/views/myview.js
+++ project/src/back-end/logs/views/myview.js
@@ -9,9 +9,46 @@ const viewDescriptor = {
            
           }
         },
+        // function sortTheList(data) {
+        //   var list = []
+        //   for (var i = 0; i < 3; i++) {
+        //     var max = 0
+        //     var index = -1
+        //     for (var j = 0; j < data.with.length; j++) {
+        //       if (list.indexOf(data.with[j]) < 0 && data.quantity[j] >= max) {
+        //         max = data.quantity[j]
+        //         index = j
+        //       }
+        //     }
+        //     if (index != -1) {
+        //       list.push(data.with[index])
+        //     }
+        
+        //   }
+        //   return list
+        // }
         reduce: function (key, values) {
-          return values
+          var data = {}
+          for(var item of values){
+            var list = []
+            for(var i=0;i<3;i++){
+              var max = 0
+              var index = -1
+              for (var j = 0; j < item.with.length; j++) {
+                if (list.indexOf(data.with[j]) < 0 && data.quantity[j] >= max) {
+                max = item.quantity[j]
+                index = j}
+              }
+            }
+            if (index != -1) {
+            list.push(data.with[index])
+
+          }
+          data[item]= list
         }
+        return data
+        }
+        
       }
     }
   }
