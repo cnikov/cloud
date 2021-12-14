42286605c8eca83fe8bdbac152351641598c0d22
chlohest <charles.lohest@student.uclouvain.be>
2021-12-14 23:18:44 +0100

    up

--- project/src/back-end/logs/views/myview.js
+++ project/src/back-end/logs/views/myview.js
@@ -3,42 +3,37 @@ const viewDescriptor = {
       logs_per_category: {
         map: function (doc) {
           if (doc['type'].localeCompare("recommendation") ==0){
-            for(var user in doc.value){
-              emit(user, doc['value'][user]);
+            var val = doc.value
+              for(var user in val){
+                var data = {}
+                for(var item in val[user]){
+                  var list = []
+                  for(var i=0;i<3;i++){
+                var max = 0
+                var index = -1
+                for (var j = 0; j <  val[user][item].with.length; j++) {
+                  if (list.indexOf( val[user][item].with[j]) < 0 &&  val[user][item].quantity[j] > max) {
+                  max =  val[user][item].quantity[j]
+                  index = j}
+                }
+                 if (index != -1) {
+              list.push( val[user][item].with[index])
             }
-           
-          }
-        },
-        reduce: function (key, values) {
-          var data = []
-          var ok = values[0]
-          return values[0]
-         for(var item in values[0]){
-           data.push(values[0][item])
-           
-         }
-         return data
-        
-          for(var item in values[0]){
-           
-            var list = []
-            
-            for(var i=0;i<3;i++){
-              var max = 0
-              var index = -1
-              for (var j = 0; j < item.with.length; j++) {
-                if (list.indexOf(item.with[j]) < 0 && item.quantity[j] >= max) {
-                max = item.quantity[j]
-                index = j}
               }
-            }
-            if (index != -1) {
-            list.push(item.with[index])
-
-          }
-          data[item.name]= list
-        }
-        return data
+             
+            data[val[user][item]['name']]= list
+                  
+               
+                  
+                }
+                 emit(user, data);
+                  
+                }
+                
+              }
+          },
+        reduce: function (key, values) {
+          return values
         }
         
       }
