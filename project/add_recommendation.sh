eb5a616adf3c35f7f5dddec5e7c66a47561ad258
chlohest <charles.lohest@student.uclouvain.be>
2021-12-14 00:47:03 +0000

    up

--- project/add_recommendation.sh
+++ project/add_recommendation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
 ip4=$(hostname -I | awk '{print $1}' )
-data=$('{"item":'${1}',"list1":'${2}',"list2":'${3}'}')
+data='"item":'${1}',"list1":'${2}',"list2":'${3}''
 echo $data
-curl -X POST -H "Content-Type: application/json" --data $data http://admin:admin@$ip4:3010/logs/recommendation
+curl -X POST -H "Content-Type: application/json" --data '{'$data'}' http://admin:admin@$ip4:3010/logs/recommendation
