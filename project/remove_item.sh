8a6ceebd7ebe592b07fc7c9224bd53e795510ce4
chlohest <charles.lohest@student.uclouvain.be>
2021-12-17 18:47:37 +0100

    up

--- project/remove_item.sh
+++ project/remove_item.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -
 ip4=$(hostname -I | awk '{print $1}' )
 curl -X DELETE http://admin:admin@$ip4:3005/format/$1
-curl -X DELETE http://admin:admin@$ip4:3005/catalog/$1
-curl -X DELETE http://admin:admin@$ip4:3005/listitem/$1
